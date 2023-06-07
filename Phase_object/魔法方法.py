24d182d2d598b30099f63e8adf39d6918fec3e32
kql_mac <kanqilu@xiaohongshu.net>
2023-06-07 18:59:06 +0800

    魔法方法

--- Phase_object/魔法方法.py
+++ Phase_object/魔法方法.py
@@ -123,3 +123,26 @@ class Foo6:
 
 obj6 = Foo6('小明', '上海')
 print(obj6)  # 输出：小明居住在上海
+
+# 8.__getitem__()、__setitem__()、__delitem__()
+'''取值、赋值、删除这“三剑客”的套路，在Python中，我们已经见过很多次了，比如前面的@property装饰器。
+Python中，标识符后面加圆括号，通常代表执行或调用方法的意思。而在标识符后面加中括号[]，通常代表取值的意思。
+Python设计了__getitem__()、__setitem__()、__delitem__()这三个特殊成员，用于执行与中括号有关的动作。
+它们分别表示取值、赋值、删除数据。如果有一个类同时定义了这三个魔法方法，那么这个类的实例的行为看起来就像一个字典一样，如下例所示：'''
+
+
+class Foo7:
+    def __getitem__(self, key):
+        print('__getitem__', key)
+
+    def __setitem__(self, key, value):
+        print('__setitem__', key, value)
+
+    def __delitem__(self, key):
+        print('__delitem__', key)
+
+
+obj7 = Foo7()
+result = obj7['k1']  # 自动触发执行 __getitem__
+obj7['k2'] = 'jack'  # 自动触发执行 __setitem__
+del obj7['k1']  # 自动触发执行 __delitem__
