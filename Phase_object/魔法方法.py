4ed3e63b21ef6f501e87ec9243a358308085ebdf
kql_mini <kql789@126.com>
2023-06-07 23:27:58 +0800

    Python魔法方法的使用

--- Phase_object/魔法方法.py
+++ Phase_object/魔法方法.py
@@ -146,3 +146,174 @@ obj7 = Foo7()
 result = obj7['k1']  # 自动触发执行 __getitem__
 obj7['k2'] = 'jack'  # 自动触发执行 __setitem__
 del obj7['k1']  # 自动触发执行 __delitem__
+
+# 9. __iter__
+'''
+这是迭代器方法！列表、字典、元组之所以可以进行for循环，是因为其内部定义了 __iter__()这个方法。
+如果用户想让自定义的类的对象可以被迭代，那么就需要在类中定义这个方法，并且让该方法的返回值是一个可迭代的对象。
+当在代码中利用for循环遍历对象时，就会调用类的这个__iter__()方法。
+'''
+
+
+class Foo8:
+    def __init__(self, sq):
+        self.sq = sq
+
+    def __iter__(self):
+        return iter(self.sq)
+
+
+obj8 = Foo8([11, 22, 33, 44])
+for i in obj8:
+    print(i)
+
+# 10.  __len__
+'''
+在Python中，如果你调用内置的len()函数试图获取一个对象的长度，在后台，其实是去调用该对象的__len__()方法，所以，下面的代码是等价的：
+Python的list、dict、str等内置数据类型都实现了该方法，但是你自定义的类要实现len方法需要好好设计。
+'''
+print(len('ABC'))
+print('ABC'.__len__())
+
+# 11. __repr__
+'''
+这个方法的作用和__str__()很像，两者的区别是__str__()返回用户看到的字符串，而__repr__()返回程序开发者看到的字符串，也就是说，__repr__()是为调试服务的。通常两者代码一样。
+'''
+
+
+class Foo11:
+    def __init__(self, city):
+        self.city = city
+
+    def __str__(self):
+        return "小明居住在%s" % self.city
+
+    __repr__ = __str__
+
+
+obj11 = Foo11("Bejing")
+print(obj11)  # 输出小明居住在Bejing
+
+# 12. 运算魔法
+'''
+__add__: 加运算 
+__sub__: 减运算 
+__mul__: 乘运算 
+__div__: 除运算 
+__mod__: 求余运算
+ __pow__: 幂运算
+ Python支持运算符的重载，也就是重写。
+'''
+
+
+class MyClass:
+
+    def __init__(self, height, weight):
+        self.height = height
+        self.weight = weight
+
+    # 两个对象的长相加，宽不变.返回一个新的类
+    def __add__(self, others):
+        return MyClass(self.height + others.height, self.weight + others.weight)
+
+    # 两个对象的宽相减，长不变.返回一个新的类
+    def __sub__(self, others):
+        return MyClass(self.height - others.height, self.weight - others.weight)
+
+    # 说一下自己的参数
+    def intro(self):
+        print("高为", self.height, " 重为", self.weight)
+
+
+def main_test():
+    a = MyClass(height=10, weight=5)
+    a.intro()
+
+    b = MyClass(height=20, weight=10)
+    b.intro()
+
+    c = b - a
+    c.intro()
+
+    d = a + b
+    d.intro()
+
+
+main_test()
+
+# 13. __auther____author__代表作者信息
+
+
+__author__ = "Kql"
+
+
+def show():
+    print(__author__)
+
+
+show()
+# 14 __slots__
+'''
+Python作为一种动态语言，可以在类定义完成和实例化后，给类或者对象继续添加随意个数或者任意类型的变量或方法，这是动态语言的特性。
+'''
+
+
+def test14(self):
+    print('hello')
+
+
+class Foo14:
+    pass
+
+
+obj14_1 = Foo14()
+obj14_2 = Foo14()
+# 动态添加实例变量
+obj14_1.name = 'kql'
+obj14_2.age = 20
+# 动态的给类添加实例方法
+Foo14.show = test14
+obj14_1.show()
+obj14_2.show()
+
+
+# 限制给实例添加的变量使用__slots__
+
+class Foo14_1:
+    __slots__ = ('name', 'age')
+    pass
+
+
+obj14_t = Foo14_1()
+obj14_w = Foo14_1()
+# 动态添加实例变量
+obj14_t.name = 'kql'
+obj14_w.age = 20
+# obj14_t.sex = 0 #会报错
+# 无法限制给类添加方法
+Foo14_1.show = test14
+obj14_t.show()
+obj14_w.show()
+
+'''
+注意：__slots__定义的属性仅对当前类的实例起作用，对继承了它的子类时不起作用的。
+若是需要对子也起作用，则需要也在子类上加上__slots__方法，这样，子类允许定义的属性就是自身的__slots__加上父类的__slots__
+'''
+
+
+class Foo15:
+    __slots__ = ('name', 'age')
+    pass
+
+
+class Foo16(Foo15):
+    print('this class is Foo16')
+    __slots__ = ('sex')
+    pass
+
+
+obj16 = Foo16()
+obj16.name = 'jack'
+obj16.age = 30
+obj16.sex = '男'
+# obj16.city = 'Beijing'  # 报错，因为父类和子类的__slots__中都没有city变量，所以是不允许添加的。
