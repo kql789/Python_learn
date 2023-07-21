7c0639defe399cd158d940d1c567e4ba5fe69044
kql_xhs <kql789@126.com>
2023-07-21 15:59:24 +0800

    算法基础

---
+++ Develop_Python/Data_structure/sort.py
@@ -0,0 +1,22 @@
+# @Author : Kql
+# @Time : 2023/7/21 13:50
+"""
+sort.py 排序算法训练
+"""
+
+
+# 冒泡排序
+def buble(list_):
+    n = len(list_)
+    # 一共循环多少轮
+    for i in range(n - 1):
+        # 每轮两两比较的次数
+        for j in range(n - 1 - i):
+            if list_[j] > list_[j + 1]:
+                list_[j], list_[j + 1] = list_[j + 1], list_[j]
+
+
+l = [4, 7, 8, 1, 3, 5, 6, 2]
+
+buble(l)
+print(l)
