36e57b87bc1c4477e435698a44ed4ded3d4c0fe2
kql_xhs <kql789@126.com>
2023-07-22 12:52:46 +0800

    快速排序代码实现

--- Develop_Python/Data_structure/sort.py
+++ Develop_Python/Data_structure/sort.py
@@ -15,11 +15,39 @@ def buble(list_):
             if list_[j] > list_[j + 1]:
                 list_[j], list_[j + 1] = list_[j + 1], list_[j]
 
+
 # 快速排序
-def quick():
-    pass
+def sub_sort(list_, low, high):
+    # 设置基准
+    x = list_[low]
+    # low向后，high向前
+    while low < high:
+        # 比基准小的数往前放
+        while list_[high] > x and low < high:
+            high -= 1
+        list_[low] = list_[high]
+        # 比基准大的数往后放
+        while list_[low] < x and low < high:
+            low += 1
+        list_[high] = list_[low]
+    # 当两者相碰撞时，将基准插入
+    list_[low] = x
+    # 返回此时的基准所在索引
+    return low
 
-l = [4, 7, 8, 1, 3, 5, 6, 2]
 
-buble(l)
-print(l)
+# 快速排序
+def quick(list_, low, high):
+    # low为有序数组中第一个元素的索引，high为最后一个数组的索引
+    if low < high:
+        key = sub_sort(list_, low, high)
+        quick(list_, low, key - 1)
+        quick(list_, key + 1, high)
+
+
+if __name__ == '__main__':
+    l = [4, 7, 8, 1, 3, 5, 6, 2]
+
+    # buble(l)  #冒泡排序
+    quick(l, 0, len(l) - 1)  # 快速排序
+    print(l)
