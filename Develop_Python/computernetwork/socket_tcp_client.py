baf47e2bce813fab1143d285c88267859a9bac45
kql_xhs <kql789@126.com>
2023-07-26 10:53:53 +0800

    modify data

--- Develop_Python/computernetwork/socket_tcp_client.py
+++ Develop_Python/computernetwork/socket_tcp_client.py
@@ -1,2 +1,15 @@
 # @Author : Kql
 # @Time : 2023/7/26 10:36
+"""
+tcp客户端流程
+"""
+import socket
+
+# 创建tcp套接字，使用默认参数——>tcp套接字
+sockfd = socket()
+
+# 服务端地址
+server_addr = ('127.0.0.1', 8888)
+# 连接服务器
+sockfd.connect(server_addr)
+
