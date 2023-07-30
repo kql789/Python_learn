3ddcd875dc60a87f63ff63d4749da7f52e206459
kql_mini <kql789@126.com>
2023-07-30 22:39:56 +0800

    多任务编程_进程

---
+++ Develop_Python/Multitask_programming/进程/fork.py
@@ -0,0 +1,23 @@
+# Author :   Kql_unicorn
+# Date   :   2023/7/30 22:17
+import os
+from time import sleep
+
+a = 1
+# 创建子进程
+pid = os.fork()
+
+if pid < 0:
+    print("Error")
+# 子进程执行部分
+elif pid == 0:
+    print("Create new proocess")
+    print("a= ", a)
+    a = 10000
+# 父进程执行部分
+else:
+    sleep(1)
+    print("The old precess")
+    print("a:", a)
+# 父子进程都会执行
+print("Fork test over")
